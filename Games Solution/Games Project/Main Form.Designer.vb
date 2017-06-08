f89b05b3611e2f02ac669cc547868d596617027a
Zachary Murphy <ztmurphy21@gmail.com>
2017-06-07 21:02:04 -0400

    some more pracitce

---
+++ Games Solution/Games Project/Main Form.Designer.vb
@@ -0,0 +1,302 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.GamesDataSet = New Games_Project.GamesDataSet()
+        Me.TblGamesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblGamesTableAdapter = New Games_Project.GamesDataSetTableAdapters.tblGamesTableAdapter()
+        Me.TableAdapterManager = New Games_Project.GamesDataSetTableAdapters.TableAdapterManager()
+        Me.TblGamesBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.TblGamesDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.txtPlatform = New System.Windows.Forms.ToolStripTextBox()
+        Me.btnGo = New System.Windows.Forms.ToolStripButton()
+        Me.btnTotal = New System.Windows.Forms.ToolStripButton()
+        CType(Me.GamesDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblGamesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblGamesBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TblGamesBindingNavigator.SuspendLayout()
+        CType(Me.TblGamesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GamesDataSet
+        '
+        Me.GamesDataSet.DataSetName = "GamesDataSet"
+        Me.GamesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TblGamesBindingSource
+        '
+        Me.TblGamesBindingSource.DataMember = "tblGames"
+        Me.TblGamesBindingSource.DataSource = Me.GamesDataSet
+        '
+        'TblGamesTableAdapter
+        '
+        Me.TblGamesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.tblGamesTableAdapter = Me.TblGamesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Games_Project.GamesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'TblGamesBindingNavigator
+        '
+        Me.TblGamesBindingNavigator.AddNewItem = Nothing
+        Me.TblGamesBindingNavigator.BindingSource = Me.TblGamesBindingSource
+        Me.TblGamesBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.TblGamesBindingNavigator.DeleteItem = Nothing
+        Me.TblGamesBindingNavigator.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.TblGamesBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.ToolStripLabel1, Me.txtPlatform, Me.btnGo, Me.btnTotal})
+        Me.TblGamesBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.TblGamesBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.TblGamesBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.TblGamesBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.TblGamesBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.TblGamesBindingNavigator.Name = "TblGamesBindingNavigator"
+        Me.TblGamesBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.TblGamesBindingNavigator.Size = New System.Drawing.Size(639, 31)
+        Me.TblGamesBindingNavigator.TabIndex = 0
+        Me.TblGamesBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(54, 28)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(28, 28)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(28, 28)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 31)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 31)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(28, 28)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(28, 28)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 31)
+        '
+        'TblGamesDataGridView
+        '
+        Me.TblGamesDataGridView.AllowUserToAddRows = False
+        Me.TblGamesDataGridView.AllowUserToDeleteRows = False
+        Me.TblGamesDataGridView.AutoGenerateColumns = False
+        Me.TblGamesDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.TblGamesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.TblGamesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6})
+        Me.TblGamesDataGridView.DataSource = Me.TblGamesBindingSource
+        Me.TblGamesDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TblGamesDataGridView.Location = New System.Drawing.Point(0, 31)
+        Me.TblGamesDataGridView.Name = "TblGamesDataGridView"
+        Me.TblGamesDataGridView.ReadOnly = True
+        Me.TblGamesDataGridView.Size = New System.Drawing.Size(639, 310)
+        Me.TblGamesDataGridView.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Title"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Title"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        Me.DataGridViewTextBoxColumn1.Width = 85
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Platform"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Platform"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Rating"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Rating"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "Price"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Price"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "NewUsed"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "NewUsed"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "Quantity"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Quantity"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        Me.DataGridViewTextBoxColumn6.ReadOnly = True
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(80, 28)
+        Me.ToolStripLabel1.Text = "&Platform"
+        '
+        'txtPlatform
+        '
+        Me.txtPlatform.Name = "txtPlatform"
+        Me.txtPlatform.Size = New System.Drawing.Size(100, 31)
+        '
+        'btnGo
+        '
+        Me.btnGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.btnGo.Image = CType(resources.GetObject("btnGo.Image"), System.Drawing.Image)
+        Me.btnGo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(28, 28)
+        Me.btnGo.Text = "&Go"
+        '
+        'btnTotal
+        '
+        Me.btnTotal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.btnTotal.Image = CType(resources.GetObject("btnTotal.Image"), System.Drawing.Image)
+        Me.btnTotal.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(28, 28)
+        Me.btnTotal.Text = "&Total Value"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(639, 341)
+        Me.Controls.Add(Me.TblGamesDataGridView)
+        Me.Controls.Add(Me.TblGamesBindingNavigator)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Games Galore"
+        CType(Me.GamesDataSet,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.TblGamesBindingSource,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.TblGamesBindingNavigator,System.ComponentModel.ISupportInitialize).EndInit
+        Me.TblGamesBindingNavigator.ResumeLayout(false)
+        Me.TblGamesBindingNavigator.PerformLayout
+        CType(Me.TblGamesDataGridView,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents GamesDataSet As GamesDataSet
+    Friend WithEvents TblGamesBindingSource As BindingSource
+    Friend WithEvents TblGamesTableAdapter As GamesDataSetTableAdapters.tblGamesTableAdapter
+    Friend WithEvents TableAdapterManager As GamesDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents TblGamesBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents TblGamesDataGridView As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As DataGridViewTextBoxColumn
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents txtPlatform As ToolStripTextBox
+    Friend WithEvents btnGo As ToolStripButton
+    Friend WithEvents btnTotal As ToolStripButton
+End Class
---
+++ Games Solution/Games Project/Main Form.vb
@@ -0,0 +1,35 @@
+﻿' Name:         Games Project
+' Purpose:      Displays all records or only those
+'               for a specific platform. Also displays
+'               the total value of the games in the store.
+' Programmer:   Zach Murphy on 6-7-2017
+
+Option Explicit On
+Option Strict On
+Option Infer On  ' using LINQ
+
+Public Class frmMain
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        'display records for a specific platform
+        Dim records = From game In GamesDataSet.tblGames
+                      Where game.Platform.ToUpper Like
+                          txtPlatform.Text.ToUpper & "*"
+                      Select game
+        TblGamesBindingSource.DataSource = records.AsDataView
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        'display the total value of the games
+        Dim dblTotal As Double =
+                                Aggregate game In GamesDataSet.tblGames
+                                    Select game.Quantity * game.Price
+                                    Into Sum
+        MessageBox.Show("Total Vale: " & dblTotal.ToString("C2"), "Game Galore", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'GamesDataSet.tblGames' table. You can move, or remove it, as needed.
+        Me.TblGamesTableAdapter.Fill(Me.GamesDataSet.tblGames)
+
+    End Sub
+End Class
